f9eae156dc2ae8cc996c4d4514d38cebb5558bf7
Manuel <mcomercio.web@gmail.com>
2019-04-18 19:43:23 -0400

    Adding instructions related with the execute_all.sh script in README and fixing errors related with the logging messages in redisReseatLoad.py3

--- pyPubSubPostgreSqlRedis/pubsub_db/execute_all.sh
+++ pyPubSubPostgreSqlRedis/pubsub_db/execute_all.sh
@@ -1,6 +1,3 @@
-# Execute from postgres user:
-# chmod +x execute_all.sh
-# sudo -i -u postgres
 psql -U postgres -a -f ./main_database.sql
 psql -U postgres -d pubsub_db -a -c "CREATE SCHEMA core_schema;"
 psql -U postgres -d pubsub_db -a -f ./core_schema/tables/tbl_brands.sql
@@ -9,4 +6,6 @@ psql -U postgres -d pubsub_db -a -f ./core_schema/tables/tbl_brands_categories.s
 psql -U postgres -d pubsub_db -a -f ./core_schema/functions/udf_brands_insert.sql
 psql -U postgres -d pubsub_db -a -f ./core_schema/functions/udf_categories_insert.sql
 psql -U postgres -d pubsub_db -a -f ./core_schema/functions/udf_brands_categories_insert.sql
-
+psql -U postgres -d pubsub_db -a -f ./core_schema/functions/udf_load_redis_brand.sql
+psql -U postgres -d pubsub_db -a -f ./core_schema/functions/udf_load_redis_categories.sql
+psql -U postgres -d pubsub_db -a -f ./core_schema/functions/udf_load_redis_brands_categories.sql
