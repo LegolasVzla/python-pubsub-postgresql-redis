6cba4e7bf72fe435ab06e4e1f53524df64debbc4
Manuel Carrero <mcomercio.web@gmail.com>
2019-05-29 18:54:02 -0400

    The Wizard installer generate this file :)

--- pubsub_db/main_postgres_db_config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo -u postgres psql -c "DROP DATABASE IF EXISTS pubsub_db;"
-sudo -u postgres psql -c "DROP USER IF EXISTS usuario1;"
-sudo -u postgres psql -c "CREATE USER usuario1 WITH PASSWORD 'postgresql';"
-sudo -u postgres psql -c "ALTER ROLE usuario1 WITH SUPERUSER;"
-sudo -u postgres psql -c "CREATE DATABASE pubsub_db WITH OWNER usuario1;"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE pubsub_db TO usuario1;"
-sudo -u postgres psql -d pubsub_db -a -c "CREATE SCHEMA core_schema;"
